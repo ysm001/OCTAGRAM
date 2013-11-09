c1b61c1e52b3d2caa4762f8e4511f02235dc9184
ryysd <ry.ysd01@gmail.com>
2013-11-09 11:11:36 +0900

    fix combat/matching page layout

--- sample_service/app/Controller/CombatsController.php
+++ sample_service/app/Controller/CombatsController.php
@@ -12,13 +12,9 @@ class CombatsController extends AppController {
         )
     );
 
-    public function beforeFilter() {
-	parent::beforeFilter();
-	$this->layout = 'bootstrap-with-header';
-    }
-
     public function index() {
         $user = $this->Auth->user();
+	$this->layout = 'bootstrap-with-header';
         $this->Paginator->settings = array(
             'limit' => 20,
             'order' => array('Program.modified' => 'desc'),
