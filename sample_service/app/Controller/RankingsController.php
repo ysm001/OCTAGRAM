e2091146d0b6645d0c257b3bf4d34325df109853
ryysd <ry.ysd01@gmail.com>
2013-11-18 10:51:02 +0900

    hide preset programs in ranking

--- sample_service/app/Controller/RankingsController.php
+++ sample_service/app/Controller/RankingsController.php
@@ -11,14 +11,16 @@ class RankingsController extends AppController {
         ));
 
     public function index() {
+        $user = $this->Auth->user();
         $this->layout = 'bootstrap-with-header';
 
         $this->Program->unbindModel(array('hasMany' => array('BattleLog')));        
         $programs = $this->Program->find('all', array(
             'limit' => 100,
-            /* 'fields' => array('Program.id', 'Program.rate'), */
             'order' => array('Program.rate' => 'desc'), 
+            'conditions' => array('Program.is_preset' => '0')
         ));
+
         $rank = 1;
         foreach ($programs as $k => $v) {
             $programs[$k]['Program']['rank'] = $rank;
@@ -29,5 +31,26 @@ class RankingsController extends AppController {
         $this->set('programs',$programs);
     }
 
+    /*
+    public function index() {
+        $user = $this->Auth->user();
+        $this->Paginator->settings = array(
+            'limit' => 20,
+            'order' => array('Program.modified' => 'desc'),
+            'conditions' => array('Program.is_preset' => '0', 'Program.user_id !=' => $user['id'])
+        );
+
+        $data = $this->Paginator->paginate('Program');
+
+	if ( isset($this->request->named['sort']) ) {
+	    $sort = $this->request->named['sort'];
+	    if ( $sort === 'Statistics.score_average' || $sort === 'Statistics.battle_num' ) {
+		$data = Set::sort($data, '{n}.'.$sort, $this->request->named['direction']);
+	    }
+	}
+
+        $this->set('programs', $data);
+    }
+     */
 }
 ?>
